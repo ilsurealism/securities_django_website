3064f283ab02f9f93dbce8ffc740e5d607e13661
ilsur <EarnMarks@yandex.ru>
2021-09-11 13:28:23 +0300

    Added files and changes required for deployment to heroku

--- website/settings.py
+++ website/settings.py
@@ -14,8 +14,6 @@ import django_heroku
 from pathlib import Path
 import os
 
-from articles.utilities import user_img_directory_path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
